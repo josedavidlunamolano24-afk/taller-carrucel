2637ec95af786d95311169b10e3a893077760954
josedavidlunamolano24-afk <josedavidlunamolano24@gmail.com>
2026-08-05 17:58:05 -0500

    se hizo la creacion del componente carrucel

---
+++ src/app/carrucel/carrucel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrucel',
+  imports: [],
+  templateUrl: './carrucel.html',
+  styleUrl: './carrucel.css',
+})
+export class Carrucel {}
