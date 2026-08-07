188ecdc2d31696af1435a13aca074976bc0db80c
Jeison-Burbano <pastasburbano204@gmail.com>
2026-08-06 19:59:30 -0500

    Revert "Yuleidi"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,3 @@
 import { Routes } from '@angular/router';
-import { Cliente } from './cliente/cliente';
-import { Proveedor } from './proveedor/proveedor';
-import { Producto } from './producto/producto'; 
-import { DetalleFactura } from './detalle-factura/detalle-factura';
-import { Factura } from './factura/factura';
 
-export const routes: Routes = [
-    {path: 'cliente', component: Cliente},
-    {path: 'proveedor', component: Proveedor},
-    {path: 'factura', component: Factura},
-    {path: 'producto', component: Producto},
-    {path: 'detalle-factura', component: DetalleFactura}
-    
-
-];
+export const routes: Routes = [];
