98cb89d707d3d6e34583356787e38cbe96cda4ca
josedavidlunamolano24-afk <josedavidlunamolano24@gmail.com>
2026-08-08 08:51:10 -0500

    se hace la correccion del codigo del crud para cliente

--- src/app/fromcliente/fromcliente.ts
+++ src/app/fromcliente/fromcliente.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-fromcliente',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './fromcliente.html',
   styleUrl: './fromcliente.css',
 })
