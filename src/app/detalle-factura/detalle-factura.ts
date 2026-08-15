f984937af8b79d87578239be0da089708d511c46
Jeison Burbano <pastasburbano204@gmail.com>
2026-08-15 13:15:09 -0500

    detalle factura queda completado y recibiendo el detalle

--- src/app/detalle-factura/detalle-factura.ts
+++ src/app/detalle-factura/detalle-factura.ts
@@ -1,5 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+
+export interface DetalleFactura{
+  id: number;
+  no_factura: number;
+  cod_producto: number;
+  cantidad_pedida: number;
+  precio_total: number;
+}
 
 @Component({
   selector: 'app-detalle-factura',
@@ -7,4 +16,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './detalle-factura.html',
   styleUrl: './detalle-factura.css',
 })
-export class DetalleFactura {}
+export class DetalleFactura {
+
+  detalles : DetalleFactura[]=[];
+  constructor(private http:HttpClient, private cdr: ChangeDetectorRef){}
+
+  ngOnInit(){
+    this.traerDetalles();
+  }
+
+  traerDetalles(){
+    this.http.get<DetalleFactura[]>('https://srrpeanqjqfxtnuwhjez.supabase.co/rest/v1/detalle_factura',
+  {
+        headers: {
+          apikey: 'sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+          authorization: 'Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x',
+          'Content-Type': 'application/json'
+        }
+  }).subscribe({next : (respuesta) => {
+    console.log(respuesta);
+    this.detalles = respuesta;
+    this.cdr.detectChanges();
+  }
+
+  })
+  }
+}
